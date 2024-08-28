299dadeae78575ce3e598af02550d8f8a70f7361
DANIEL FS <144292601+nicolas4411@users.noreply.github.com>
2024-08-27 21:41:18 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,30 @@
+resposta 1
+
+ const quadrado = num => num * num;
+console.log(quadrado(3));
+
+resposta 2 
+
+
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(4, 3));
+
+resposta 3
+
+
+const saudacao = () => 'ola Daniel';
+console.log(saudacao());
+
+resposta 4
+
+const multiplicarPorDois = (num) => {
+    let resultado = num * 2;
+    return resultado;
+}
+console.log( multiplicarPorDois(7));
+
+
+
+
